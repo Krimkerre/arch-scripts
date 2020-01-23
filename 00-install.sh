0bbd45053c566529279c6811e86b987f4a46bd23
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-01-22 22:27:14 -0800

    Add files via upload

--- 00-install.sh
+++ 00-install.sh
@@ -72,7 +72,8 @@ echo "LANG=${alocale}" > /mnt/etc/locale.conf
 echo "${ahostname}" > /mnt/etc/hostname
 sed -i 's/^#\ \(%wheel\ ALL=(ALL)\ NOPASSWD:\ ALL\)/\1/' /mnt/etc/sudoers
 echo 'KEYMAP='"${akeymap}" > /mnt/etc/vconsole.conf
-echo 'FONT='"${deffnt}" > /mnt/etc/vconsole.conf
+sudo sed -i "$ a FONT=${deffnt}" /etc/vconsole.conf
+#echo 'FONT='"${deffnt}" > /mnt/etc/vconsole.conf
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/${timezne} /etc/localtime
 
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
