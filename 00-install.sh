372059a3069065f5e8528e1214b11d720458d5f9
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-01-28 11:12:15 -0800

    Add files via upload

--- 00-install.sh
+++ 00-install.sh
@@ -59,6 +59,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 if [[ -d /sys/firmware/efi/efivars ]]; then
   arch-chroot /mnt bootctl install
   partid=$(blkid -s PARTUUID -o value /dev/${drive}2)
+
   {
     echo default arch
     echo 'timeout 2'
