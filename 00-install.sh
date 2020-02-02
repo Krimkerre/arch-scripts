0678b53c05f4631d288a99166ae741e6c8017bd6
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-02-02 07:19:02 -0800

    Add files via upload

--- 00-install.sh
+++ 00-install.sh
@@ -34,13 +34,6 @@ timezne='America/Los_Angeles'
 user=$(dialog --stdout --inputbox "Enter username" 0 0) || exit 1
 clear
 : ${user:?"user cannot be empty"}
-# Admin password
-passwordroot=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
-clear
-: ${passwordroot:?"password cannot be empty"}
-passwordroot2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
-clear
-[[ "$passwordroot" == "$passwordroot2" ]] || ( echo "Passwords did not match"; exit 1; )
 # User password
 password=$(dialog --stdout --passwordbox "Enter user password" 0 0) || exit 1
 clear
@@ -48,7 +41,13 @@ clear
 password2=$(dialog --stdout --passwordbox "Enter user password again" 0 0) || exit 1
 clear
 [[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
-
+# Admin password
+passwordroot=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
+clear
+: ${passwordroot:?"password cannot be empty"}
+passwordroot2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
+clear
+[[ "$passwordroot" == "$passwordroot2" ]] || ( echo "Passwords did not match"; exit 1; )
 
 ######################################################################################
 timedatectl set-ntp true
