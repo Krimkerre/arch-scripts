e16e67efc7180bf32b1bc031c59b2616459098fe
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-02-04 19:29:16 -0800

    Add files via upload

--- 00-install.sh
+++ 00-install.sh
@@ -158,7 +158,7 @@ $passwordroot" | arch-chroot /mnt passwd
 
 ##### Copy the GIT scripts to user directory #########################################
 cp *.sh /mnt/home/$user/
-chown $user:users /mnt/home/$user/*.sh
+arch-chroot chown $user:users /home/$user/*.sh
 
 #####################################################################################
 
