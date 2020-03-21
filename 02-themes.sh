e998aa571cdba06e7dccdb0196b2bf3b106ef441
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-03-20 21:52:30 -0700

    Add files via upload

--- 02-themes.sh
+++ 02-themes.sh
@@ -28,10 +28,10 @@ yay -S --noconfirm --needed chicago95-gtk-theme-git
 yay -S --noconfirm --needed pro-dark-gtk-theme-git
 yay -S --noconfirm --needed aqua-git
 yay -S --noconfirm --needed evopop-gtk-theme-git
-yay -S --noconfirm --needed chromeos-gtk-theme-git
 yay -S --noconfirm --needed gtk-theme-inspire-ui
 yay -S --noconfirm --needed qogir-gtk-theme-git
 yay -S --noconfirm --needed candy-gtk-theme
+#yay -S --noconfirm --needed chromeos-gtk-theme-git
 #yay -S --noconfirm --needed mojave-gtk-theme-git
 #yay -S --noconfirm --needed pop-gtk-theme-git
 #yay -S --noconfirm --needed canta-gtk-theme
