b118ed1ae63ac0de1c7a522cdbe09c348e45d31c
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-04-28 05:58:40 -0700

    Add files via upload

--- 01-setup.sh
+++ 01-setup.sh
@@ -349,10 +349,10 @@ case $case in
       yay -S --noconfirm --needed xfce4-screensaver
       yay -S --noconfirm --needed xfce4-panel-profiles-git
       yay -S --noconfirm --needed mugshot
-      yay -S --noconfirm --needed compton-conf-git
       yay -S --noconfirm --needed solarized-dark-themes
       yay -S --noconfirm --needed gtk-theme-glossyblack
       yay -S --noconfirm --needed mcos-mjv-xfce-edition
+      #yay -S --noconfirm --needed compton-conf-git
       #yay -S --noconfirm --needed xfce-theme-manager
       sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
       sudo pacman -S --noconfirm --needed lightdm-webkit-theme-litarvan
