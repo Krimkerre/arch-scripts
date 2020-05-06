af0a818b1d5874471a4deba0c717fdba36c4e516
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-05-06 08:37:28 -0700

    Add files via upload

--- 01-setup.sh
+++ 01-setup.sh
@@ -523,6 +523,7 @@ echo "##########################################################################
 sleep 2
 yay -S --noconfirm --needed pamac-aur-git
 yay -S --noconfirm --needed snapd-git
+sudo pacman -S --noconfirm --needed flatpak
 sudo systemctl enable snapd.service
 yay -S --noconfirm --needed bauh
 
