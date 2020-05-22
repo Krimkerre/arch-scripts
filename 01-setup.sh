48f6d834291071d9828e6ef917c10517253d81b9
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-05-22 16:26:21 -0700

    Add files via upload

--- 01-setup.sh
+++ 01-setup.sh
@@ -318,7 +318,7 @@ case $case in
       sudo pacman -S --noconfirm --needed gnome-disk-utility
       sudo pacman -S --noconfirm --needed variety
       sudo pacman -S --noconfirm --needed onboard
-      yay -S --noconfirm --needed user-admin
+      #yay -S --noconfirm --needed user-admin
       yay -S --noconfirm --needed mate-tweak
       yay -S --noconfirm --needed brisk-menu
       yay -S --noconfirm --needed mate-screensaver-hacks
