c1202da2b480729be311f1c4b080301343758c08
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-07-03 07:37:40 -0700

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -157,9 +157,9 @@ function SYSDSWAP() {
   echo "zswap_max_pool_percent=25" >> /mnt/etc/systemd/swap.conf
   echo "zswap_zpool=z3fold" >> /mnt/etc/systemd/swap.conf
   echo "zram_enabled=1"  >> /mnt/etc/systemd/swap.conf
-  echo "zram_size=$(( RAM_SIZE / 4 ))"  >> /mnt/etc/systemd/swap.conf
-  echo "zram_count=${NCPU}"  >> /mnt/etc/systemd/swap.conf
-  echo "zram_streams=${NCPU}" >> /mnt/etc/systemd/swap.conf
+  echo "zram_size=\$(( RAM_SIZE / 4 ))"  >> /mnt/etc/systemd/swap.conf
+  echo "zram_count=\${NCPU}"  >> /mnt/etc/systemd/swap.conf
+  echo "zram_streams=\${NCPU}" >> /mnt/etc/systemd/swap.conf
   echo "zram_alg=zstd" >> /mnt/etc/systemd/swap.conf
   echo "zram_prio=32767" >> /mnt/etc/systemd/swap.conf
   echo "swapfc_enabled=1" >> /mnt/etc/systemd/swap.conf
