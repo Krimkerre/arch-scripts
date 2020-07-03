46794a80a14c364e0c863466f87321fb07cb7601
lotw69 <53122963+lotw69@users.noreply.github.com>
2020-07-02 18:27:24 -0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -64,9 +64,61 @@ function ADMN() {
   [[ "$PASSWORDROOT" == "$PASSWORDROOT2" ]] || ( echo "Passwords did not match"; exit 1; )
 }
 
+function FMTDRV() {
+  sgdisk -Z ${drive}
 
+  if [[ -d /sys/firmware/efi/efivars ]]; then
+    #UEFI Partition
+    parted ${drive} mklabel gpt mkpart primary fat32 1MiB 301MiB set 1 esp on mkpart primary ext4 301MiB 100%
+    mkfs.fat -F32 ${drive}1
+    mkfs.ext4 ${drive}2
+    #mkfs.btrfs -f ${drive}2
+    mount ${drive}2 /mnt
+    mkdir /mnt/boot
+    mount ${drive}1 /mnt/boot
+
+  else
+    #BIOS Partition
+    parted ${drive} mklabel msdos mkpart primary ext4 2MiB 100% set 1 boot on
+    mkfs.ext4 ${drive}1
+    #mkfs.btrfs ${drive}1
+    mount ${drive}1 /mnt
+  fi
+}
+
+function BASEPKG() {
+  pacstrap /mnt base base-devel linux linux-firmware linux-headers nano networkmanager
+  genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+function SYSD_BOOT() {
+  arch-chroot /mnt mkdir -p /boot/loader/entries
+  arch-chroot /mnt bootctl --path=/boot install
+
+  echo "default arch" >> /mnt/boot/loader/loader.conf
+  echo "timeout 3" >> /mnt/boot/loader/loader.conf
+  echo "console-mode max" >> /mnt/boot/loader/loader.conf
+  echo "editor no" >> /mnt/boot/loader/loader.conf
+
+  echo "title Arch Linux" >> /mnt/boot/loader/entries/arch.conf
+  echo "linux /vmlinuz-linux" >> /mnt/boot/loader/entries/arch.conf
+  echo "#initrd  /intel-ucode.img" >> /mnt/boot/loader/entries/arch.conf
+  echo "#initrd /amd-ucode.img" >> /mnt/boot/loader/entries/arch.conf
+  echo "initrd  /initramfs-linux.img" >> /mnt/boot/loader/entries/arch.conf
+  echo "options root=PARTUUID=$(blkid -s PARTUUID -o value "$drive"2) nowatchdog rw" >> /mnt/boot/loader/entries/arch.conf
+
+}
+
+################################################################################
+### Main program                                                             ###
+################################################################################
 timedatectl set-ntp true
 TIMEZONES
 CNTRY
 HSTNAME
+clear
 PACSET
+DRVTOUSE
+clear
+FMTDRV
+SYSD_BOOT
