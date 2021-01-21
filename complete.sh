a5ef5cdf08daf0b2757982169ee098cf3213f86a
lotw69 <53122963+lotw69@users.noreply.github.com>
2021-01-21 01:32:27 -0800

    Add files via upload

--- complete.sh
+++ complete.sh
@@ -906,7 +906,7 @@ function PACMAN_KEYS() {
   sleep 2
   sudo pacman-key --init
   sudo pacman-key --populate archlinux
-  sudo reflector --country US --latest 20 --sort rate --verbose --save /etc/pacman.d/mirrorlist
+  sudo reflector --country US --age 6 --sort rate --verbose --save /etc/pacman.d/mirrorlist
   sudo pacman -Sy
 }
 ################################################################################
