d723361ae44aca00e0d99a67a6f0bcea9f062591
lotw69 <53122963+lotw69@users.noreply.github.com>
2021-06-04 10:44:56 -0700

    Add files via upload

--- base2.sh
+++ base2.sh
@@ -145,7 +145,7 @@ function NEEDED_INSTALL() {
   echo "##############################################################################"
   sleep 3
   sed -i 's/\#ParallelDownloads = 5/ParallelDownloads = 5'/g /etc/pacman.conf
-  sed '/^#Color\I Love Candy'/g /etc/pacman.conf
+  sed -i 's/Color/I Love Candy/'/g /etc/pacman.conf
   pacman -S --noconfirm --needed dialog
 }
 
