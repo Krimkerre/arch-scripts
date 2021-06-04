53b5ab82c8f60cc0dd6e25981dab12cd89314b0d
lotw69 <53122963+lotw69@users.noreply.github.com>
2021-06-04 12:50:52 -0700

    Add files via upload

--- complete2.sh
+++ complete2.sh
@@ -117,7 +117,7 @@ function UNICODEFIX() {
 ################################################################################
 function NEEDED_SOFTWARE() {
   clear
-  dialog --infobox "Adding Some Needed Software." 5 28
+  dialog --infobox "Adding Some Needed Software." 5 30
   sleep 2
   $ZB -S --noconfirm --needed tuned duf fontpreview-ueberzug-git ytfzf cpufetch buttermanager
 }
